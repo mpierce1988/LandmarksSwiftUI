6b5f392ea0a08fb9040296ea095466029e0b59eb
Michael Pierce <michael.todd.pierce@gmail.com>
2023-06-17 19:11:31 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Michael Pierce on 2023-06-17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
