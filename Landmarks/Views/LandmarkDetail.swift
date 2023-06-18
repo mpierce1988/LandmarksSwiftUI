6a36a88c6448d709d099892d8a17b3f65a263313
Michael Pierce <michael.todd.pierce@gmail.com>
2023-06-17 21:40:17 -0300

    Added navigation title and navigation bar display type to details view

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -39,6 +39,8 @@ struct LandmarkDetail: View {
             .padding()
             Spacer()
         }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
