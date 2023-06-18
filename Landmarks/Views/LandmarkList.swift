103e36473ba285d13fe1c9f8c5cead872828c196
Michael Pierce <michael.todd.pierce@gmail.com>
2023-06-17 21:52:23 -0300

    Added more preview devices

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -24,6 +24,17 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+
+        ForEach(["iPhone 14 Pro Max", "iPhone SE (3rd generation)", "iPad Pro (12.9-inch) (6th generation)"], id: \.self) { deviceName in
+
+            LandmarkList()
+                
+
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+
+                .previewDisplayName(deviceName)
+
+        }
+
     }
 }
