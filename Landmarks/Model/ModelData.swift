7c2427546495d5509b9943eeeb4ad42b86c71b84
Michael Pierce <michael.todd.pierce@gmail.com>
2023-06-17 22:41:07 -0300

    Added ModelData observable object to ModelData.swift

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -7,7 +7,11 @@
 
 import Foundation
 
-var landmarks: [Landmark] = load("landmarkData.json")
+final class ModelData: ObservableObject {
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+}
+
+
 
 func load<T: Decodable>(_ filename: String) -> T {
     // variable to hold data from file
