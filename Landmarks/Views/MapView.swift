107e46196a08c5727fee15b19849807706f41cae
Michael Pierce <michael.todd.pierce@gmail.com>
2023-06-17 21:33:26 -0300

    Modified MapView to take a coordinate as a parameter, set the region for the map onAppear

--- Landmarks/Views/MapView.swift
+++ Landmarks/Views/MapView.swift
@@ -9,23 +9,30 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    var coordinate: CLLocationCoordinate2D
     
     // Source of "truth" that can be accessed and modified from other views
     // SwiftUI manages underlying storage and updating of views that depend on this value
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-    )
+    @State private var region = MKCoordinateRegion()
     
     var body: some View {
         // $ passes a binding to the region state variable
         // when the user modifies the position on the map, it updates the region variable
         Map(coordinateRegion: $region)
+            .onAppear {
+                setRegion(coordinate)
+            }
+    }
+    
+    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(
+            center: coordinate, span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
     }
 }
