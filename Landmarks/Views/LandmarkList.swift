00631dba8c95fc5064f91987c9e3d998eee00de9
Michael Pierce <michael.todd.pierce@gmail.com>
2023-06-17 21:24:03 -0300

    Put LandmarkList list inside NavigationView, add title

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List(landmarks) { landmark in
-            LandmarkRow(landmark: landmark)
-        }
+        NavigationView {
+            List(landmarks) { landmark in
+                LandmarkRow(landmark: landmark)
+            }
+            .navigationTitle("Landmarks")
+        }        
     }
 }
 
