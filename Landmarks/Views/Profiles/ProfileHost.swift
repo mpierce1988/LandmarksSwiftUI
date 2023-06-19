6ce6e286331d7f6df9146ca0eb11b501e0b54803
Michael Pierce <michael.todd.pierce@gmail.com>
2023-06-19 17:59:54 -0300

    User Profile complete

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Michael Pierce on 2023-06-19.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
